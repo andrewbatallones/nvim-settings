806045632322ca20b48e2ef957d0497088929097
Andrew Batallones <batallonesa@gmail.com>
2023-06-01 10:57:31 -0400

    use double quotes

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,15 +3,17 @@
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
   -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+  use "wbthomason/packer.nvim"
 
+  -- Treesitter
   use {
-        'nvim-treesitter/nvim-treesitter',
+        "nvim-treesitter/nvim-treesitter",
         run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
             ts_update()
         end,
     }
+
 end)
