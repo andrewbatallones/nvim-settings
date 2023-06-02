a8629c359ebf59a113861463a793d3a27107f2ee
Andrew Batallones <batallonesa@gmail.com>
2023-06-02 12:43:29 -0400

    replace ' with "

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,4 +1,4 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require("plugins")` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -19,23 +19,23 @@ return require("packer").startup(function(use)
   use "folke/tokyonight.nvim"
 
   use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
     requires = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
+      {"neovim/nvim-lspconfig"},             -- Required
       {                                      -- Optional
-	'williamboman/mason.nvim',
+	"williamboman/mason.nvim",
 	run = function()
-	  pcall(vim.cmd, 'MasonUpdate')
+	  pcall(vim.cmd, "MasonUpdate")
 	end,
       },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      {"williamboman/mason-lspconfig.nvim"}, -- Optional
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
+      {"hrsh7th/nvim-cmp"},     -- Required
+      {"hrsh7th/cmp-nvim-lsp"}, -- Required
+      {"L3MON4D3/LuaSnip"},     -- Required
     }
   }
 
