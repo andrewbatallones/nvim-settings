d6a5daacc032c03bb62dbe1a15c1298d4f81eee4
Andrew Batallones <batallonesa@gmail.com>
2023-06-02 16:43:11 -0400

    update keymaps

--- lua/andrew/keymaps.lua
+++ lua/andrew/keymaps.lua
@@ -1,3 +1,10 @@
+-- leader map has to exists in order for <leader to work>
+vim.g.mapleader = " "
+
 -- It looks like on the rhs, you don't specify the <leader> cmd, but rather ":" itself
-vim.keymap.set("n", "-", ":Ex<cr>", { silent = true });
-vim.keymap.set("n", "<C-s>", ":w<cr>", { silent = true });
+vim.keymap.set("n", "-", ":Ex<cr>", { silent = true })
+vim.keymap.set("n", "<C-s>", ":w<cr>", { silent = true })
+
+-- Telescope
+vim.keymap.set("n", "<leader>f", ":Telescope find_files<cr>")
+vim.keymap.set("n", "<leader>F", ":Telescope live_grep<cr>")
