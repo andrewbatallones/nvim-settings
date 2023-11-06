cbf7f5a46f1e2c441e62a8a0518e1478447d42b6
Andrew Batallones <abatallones@bradsdeals.com>
2023-11-06 11:45:11 -0500

    remove custom clipboard logic

--- lua/andrew/options.lua
+++ lua/andrew/options.lua
@@ -3,17 +3,3 @@ vim.opt.number = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.termguicolors = true
-
--- This only works for WSL
-vim.g.clipboard = {
-   name = 'WslClipboard',
-   copy = {
-      ["+"] = 'clip.exe',
-      ["*"] = 'clip.exe',
-    },
-   paste = {
-	   ["+ "]= 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-   },
-   cache_enabled = 0,
- }
