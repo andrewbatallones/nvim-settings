8de7b758c39dfd565d9e32c9cbadce90cc239bd7
andrewbatallones <batallonesa@gmail.com>
2024-04-11 09:03:59 -0400

    update and document keymaps

--- lua/andrew/keymaps.lua
+++ lua/andrew/keymaps.lua
@@ -1,9 +1,8 @@
 -- leader map has to exists in order for <leader to work>
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "-", ":Ex<cr>", { silent = true })
-vim.keymap.set("n", "<C-s>", ":w<cr>", { silent = true })
-vim.keymap.set("v", "<leader>y", "\"*y", { silent = true })
+vim.keymap.set("n", "-", ":Ex<cr>", { silent = true }) -- This just lets me go back a buffer
+vim.keymap.set("v", "<leader>y", "\"*y", { silent = true }) -- Shortcut to copy text to clipboard
 
 -- Want to completely ignore middlemouse pasting.
 vim.keymap.set("n", "<MiddleMouse>", "<Nop>", { silent = true })
@@ -21,11 +20,8 @@ vim.keymap.set("n", "<C-l>", "<C-w>l", { silent = true })
 vim.keymap.set("n", "<leader>f", ":Telescope find_files<cr>")
 vim.keymap.set("n", "<leader>F", ":Telescope live_grep<cr>")
 
--- nvim-tree
-vim.keymap.set("n", "<leader>t", ":NvimTreeToggle<cr>", { silent = true })
-
 -- lsp
 vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float()<cr>")
 
 -- null-ls
-vim.keymap.set("n", "<leader>s", ":lua vim.lsp.buf.format()<cr>")
+vim.keymap.set("n", "<leader>s", ":lua vim.lsp.buf.format()<cr>") -- This autoformats code
